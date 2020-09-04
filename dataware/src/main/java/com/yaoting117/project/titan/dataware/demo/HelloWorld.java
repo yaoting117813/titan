18b57b3b1fda0170e5355ffe6e8d5fc84f13cc49
yaoting117 <43172627@qq.com>
2020-09-04 10:17:55 +0800

    添加测试代码

--- dataware/src/main/java/com/yaoting117/project/titan/dataware/demo/HelloWorld.java
+++ dataware/src/main/java/com/yaoting117/project/titan/dataware/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.yaoting117.project.titan.dataware.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("胡倩倩");
     }
 }
